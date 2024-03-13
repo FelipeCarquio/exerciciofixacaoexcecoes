d5d8377828cfb34d51d2b058f59096ace4db9be0
FelipeCarquio <flipkrquio@gmail.com>
2024-03-13 09:49:05 -0300

    1ª Solução do exercicio tratamento de exceções

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,48 @@
 package application;
 
+import model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        Scanner sc = new Scanner(System.in);
+
+        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room Number: ");
+        int number = sc.nextInt();
+        System.out.print("Check-in date (DD/MM/YYYY): ");
+        Date checkin = df.parse(sc.next());
+        System.out.print("Check-out date (DD/MM/YYYY): ");
+        Date checkout = df.parse(sc.next());
+
+        if (!checkout.after(checkin)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        } else {
+            Reservation reservation = new Reservation(number, checkin,checkout);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println();
+            System.out.println("Enter data to update the reservation:");
+            System.out.print("Check-in date (DD/MM/YYYY): ");
+            checkin = df.parse(sc.next());
+            System.out.print("Check-out date (DD/MM/YYYY): ");
+            checkout = df.parse(sc.next());
+
+            Date now = new Date();
+            if (checkin.before(now) || checkout.before(now)) {
+                System.out.println("Error in reservation: Reservation dates for update must be future dates.");
+            } else if (!checkout.after(checkin)) {
+                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            } else {
+                reservation.updateDates(checkin, checkout);
+                System.out.println("Reservation: " + reservation);
+            }
+        }
+    }
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,4 +1,58 @@
 package model.entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Reservation {
+
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+    //para calcular os dias entre datas.
+    public Long duration() {
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public String toString() {
+        return "Rooms "
+                + roomNumber
+                + ", Check-in: "
+                + dateFormat.format(checkIn)
+                + ", Check-out: "
+                + dateFormat.format(checkOut)
+                + ", "
+                + duration()
+                + " nights.";
+    }
 }
